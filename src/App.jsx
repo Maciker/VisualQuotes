5867f074daf352a3eff0d72a86b7044bbbdcfcbd
maciker <ikermacaya@gmail.com>
2023-10-05 23:37:35 +0200

    Creating the grid and the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import {Box, ChakraProvider, Container, Heading, ListItem, UnorderedList} from "@chakra-ui/react";
+import {
+    ChakraProvider,
+    Container,
+    Heading,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Grid,
+    Image,
+    GridItem
+} from "@chakra-ui/react";
 import {useState} from "react";
-import { Image } from '@chakra-ui/react'
 
 function App() {
     const [quotes, setQuotes] = useState([
@@ -10,28 +20,45 @@ function App() {
             "author": "Francis Bacon",
             "visual": "https://cdn.midjourney.com/6cf63138-02f2-4b6f-b682-38ab2e52dac0/0_2.webp",
             "prompt": "knowledge is power, in minimalist style"
-        }
+        },
+        {
+            "id": 2,
+            "quote": "Unax talking with the moon",
+            "author": "Francis Bacon",
+            "visual": "https://cdn.midjourney.com/69bd4e55-6fa9-48e6-81d9-cc1525feada5/0_0.webp",
+            "prompt": "a two years blonde kid, with short hair, talking with the moon"
+        },
     ])
 
 
   return (
     <ChakraProvider>
-        <Container mt={10}>
+        <Container mt={10} centerContent>
             <Heading>Visual Quotes</Heading>
-            <UnorderedList>
+        </Container>
+            <Grid templateColumns='repeat(3, 1fr)' gap={25}>
                 {quotes.map(quote => {
                     return (
-                        <Box key={quote.id}>
-                            <ListItem key={quote.id}>{quote.quote}</ListItem>
-                            <ListItem key={quote.id}>{quote.prompt}</ListItem>
-                            <Box boxSize='sm'>
-                                <Image src={quote.visual} alt={quote.quote} />
-                            </Box>
-                        </Box>
+                        <GridItem w='100%' h='100%' key={quote.id}>
+                            <Card key={quote.id} w='100%' h='100%'>
+                                <CardHeader>
+                                    <Heading size='md'>{quote.quote}</Heading>
+                                </CardHeader>
+                                <CardBody>
+                                    <Image
+                                        src={quote.visual}
+                                        alt={quote.quote}
+                                        borderRadius='lg'
+                                    />
+                                </CardBody>
+                                <CardFooter>
+                                    <Heading size='xs'>{quote.prompt}</Heading>
+                                </CardFooter>
+                            </Card>
+                        </GridItem>
                     )}
                 )}
-            </UnorderedList>
-        </Container>
+            </Grid>
     </ChakraProvider>
   )
 }
